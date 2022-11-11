5a528a878aebfb0e5649cf188e314282d6669422
Qredo LTD <support@qredo.com>
2022-11-11 16:35:24 +0800

    Format

--- app/Commands.hs
+++ app/Commands.hs
@@ -1,78 +1,80 @@
 {-# LANGUAGE RecordWildCards #-}
+
 module Commands where
 
+import Control.Monad
+import Data.Aeson qualified as Aeson
+import Data.ByteString.Lazy qualified as LBS
+import Data.List (isPrefixOf)
+import Data.Text.Lazy qualified as T
+import Data.Word
+import GHC.Generics
+import Language.Wasm qualified as Wasm
 import MASM (ppMASM)
 import MASM.Miden (runMidenVerify)
 import Options
-import Validation
-import W2M
-
-import Control.Monad
-import Data.List (isPrefixOf)
-import Data.Word
-import GHC.Generics
 import System.Exit
 import System.FilePath
 import System.IO
 import System.IO.Temp
 import System.Process
 import Text.Pretty.Simple (pShow)
-
-import qualified Data.Aeson as Aeson
-import qualified Data.ByteString.Lazy as LBS
-import qualified Data.Text.Lazy as T
-import qualified Language.Wasm as Wasm
+import Validation
+import W2M
 
 runCommand :: Command -> IO ()
-runCommand (Build  buildOpts)  = runBuild  buildOpts
-runCommand (Run    runOpts)    = runRun    runOpts
+runCommand (Build buildOpts) = runBuild buildOpts
+runCommand (Run runOpts) = runRun runOpts
 runCommand (Verify verifyOpts) = runVerify verifyOpts
 
 ---------------
 
 runBuild :: BuildOpts -> IO ()
-runBuild BuildOpts{..} = withWasm buildInFile $ \wasmFile wasmMod -> do
-    when dumpWasm $ case wasmFile of
-        DotWat fp -> dumpFile "WASM code" fp
-        DotWasm fp -> withWatFromWasm fp $ \watFP ->
-            dumpFile "WASM code" watFP
+runBuild BuildOpts {..} = withWasm buildInFile $ \wasmFile wasmMod -> do
+  when dumpWasm $ case wasmFile of
+    DotWat fp -> dumpFile "WASM code" fp
+    DotWasm fp -> withWatFromWasm fp $ \watFP ->
+      dumpFile "WASM code" watFP
 
-    when dumpWasmAst $
-      dumps "WASM AST" (lines . T.unpack $ pShow wasmMod)
+  when dumpWasmAst $
+    dumps "WASM AST" (lines . T.unpack $ pShow wasmMod)
 
-    masmMod <- runValidation $ do
-      standardValidator wasmMod
-      toMASM wasmMod
+  masmMod <- runValidation $ do
+    standardValidator wasmMod
+    toMASM wasmMod
 
-    when dumpMasmAst $
-      dumps "MASM AST" (lines . T.unpack $ pShow masmMod)
+  when dumpMasmAst $
+    dumps "MASM AST" (lines . T.unpack $ pShow masmMod)
 
-    let masmCode = ppMASM masmMod
+  let masmCode = ppMASM masmMod
 
-    writeFile buildOutMasmFile masmCode
+  writeFile buildOutMasmFile masmCode
 
-    when dumpMasm $
-      dumpFile "MASM code" buildOutMasmFile
+  when dumpMasm $
+    dumpFile "MASM code" buildOutMasmFile
 
-    when brunToo $
-      runRun $
-        RunOpts { runMasmFile = buildOutMasmFile
-                , runProofFile = buildOutMasmFile <.> "proof"
-                , runOutFile = buildOutMasmFile <.> "out"
-                , rverifyToo = bverifyToo
-                }
-
-  where standardValidator wasm_mod =
-          case Wasm.validate wasm_mod of
-            Left err -> failsStandardValidation err
-            Right _validMod -> return ()
+  when brunToo $
+    runRun $
+      RunOpts
+        { runMasmFile = buildOutMasmFile,
+          runProofFile = buildOutMasmFile <.> "proof",
+          runOutFile = buildOutMasmFile <.> "out",
+          rverifyToo = bverifyToo
+        }
+  where
+    standardValidator wasm_mod =
+      case Wasm.validate wasm_mod of
+        Left err -> failsStandardValidation err
+        Right _validMod -> return ()
 
 runRun :: RunOpts -> IO ()
-runRun RunOpts{..} = do
+runRun RunOpts {..} = do
   dump ("Execution of program " ++ runMasmFile ++ " ...")
   (ex, midenout, midenerr) <-
-    readProcessWithExitCode "miden"
-      ["prove", "--assembly", runMasmFile, "-o", runOutFile, "-p", runProofFile] ""
+    readProcessWithExitCode
+      "miden"
+      ["prove", "--assembly", runMasmFile, "-o", runOutFile, "-p", runProofFile]
+      ""
   case ex of
     ExitFailure n -> do
       dump ("miden prove failed with exit code: " ++ show n)
@@ -80,32 +82,35 @@ runRun RunOpts{..} = do
       dumps "miden prove stderr" (lines midenerr)
       error "miden prove failed"
     ExitSuccess ->
-      case map (takeWhile (/='.') . drop (length hashPrefix)) $ filter (hashPrefix `isPrefixOf`) (lines midenout) of
+      case map (takeWhile (/= '.') . drop (length hashPrefix)) $ filter (hashPrefix `isPrefixOf`) (lines midenout) of
         [hash] -> do
           ok (runOutFile, runProofFile, hash)
           when rverifyToo $
-            runVerify $ VerifyOpts { verifyOutFile = runOutFile
-                                   , verifyProofFile = runProofFile
-                                   , verifyHash = hash
-                                   }
+            runVerify $
+              VerifyOpts
+                { verifyOutFile = runOutFile,
+                  verifyProofFile = runProofFile,
+                  verifyHash = hash
+                }
         _ -> do
           dumps "miden prove output" (lines midenout)
           error "couldn't determine hash from miden prove output"
+  where
+    hashPrefix = "Proving program with hash "
 
-  where hashPrefix = "Proving program with hash "
-
-        ok (out, proof, hash) = do
-          stack <- getStack out
-          putStrLn $ unlines
-            [ "Successfullt generated proof " ++ proof
-            , "Output of the program stored in " ++ out
-            , "Program hash: " ++ hash
-            , "Final state of the stack:"
-            , "\t" ++ show stack
-            ]
+    ok (out, proof, hash) = do
+      stack <- getStack out
+      putStrLn $
+        unlines
+          [ "Successfullt generated proof " ++ proof,
+            "Output of the program stored in " ++ out,
+            "Program hash: " ++ hash,
+            "Final state of the stack:",
+            "\t" ++ show stack
+          ]
 
 runVerify :: VerifyOpts -> IO ()
-runVerify VerifyOpts{..} = do
+runVerify VerifyOpts {..} = do
   dump ("Verification of proof " ++ verifyProofFile ++ " ...")
   r <- runMidenVerify verifyOutFile verifyProofFile verifyHash
   case r of
@@ -117,9 +122,10 @@ runVerify VerifyOpts{..} = do
       error "miden verify failed"
 
 data MidenOut = MidenOut
-  { stack :: [String]
-  , overflow_addrs :: [String]
-  } deriving (Show, Generic)
+  { stack :: [String],
+    overflow_addrs :: [String]
+  }
+  deriving (Show, Generic)
 
 instance Aeson.FromJSON MidenOut
 
@@ -132,73 +138,85 @@ getStack fp = do
 
 ---------------
 
-withCompiledC
- :: FilePath -- path to .c file
- -> (FilePath -> IO a) -- what to do with .wasm file
- -> IO a
+withCompiledC ::
+  FilePath -> -- path to .c file
+  (FilePath -> IO a) -> -- what to do with .wasm file
+  IO a
 withCompiledC dotc operation = do
-    withSystemTempDirectory "withCompiledC" $ \dir  -> do
-      let wasmPath = dir </> takeFileName dotc <.> "wasm"
-      (ex, clangout, clangerr) <- readProcessWithExitCode "clang-14"
-        (clangOpts ++ ["-o", wasmPath, dotc]) ""
-      case ex of
-              ExitSuccess -> operation wasmPath
-              ExitFailure e -> error $
-                "clang failed: " ++ show (e, clangout, clangerr)
+  withSystemTempDirectory "withCompiledC" $ \dir -> do
+    let wasmPath = dir </> takeFileName dotc <.> "wasm"
+    (ex, clangout, clangerr) <-
+      readProcessWithExitCode
+        "clang-14"
+        (clangOpts ++ ["-o", wasmPath, dotc])
+        ""
+    case ex of
+      ExitSuccess -> operation wasmPath
+      ExitFailure e ->
+        error $
+          "clang failed: " ++ show (e, clangout, clangerr)
+  where
+    clangOpts =
+      [ "--target=wasm32",
+        "--no-standard-libraries",
+        "-O1",
+        "-Wl,--no-entry",
+        "-Wl,--export-all",
+        "-Wl,--strip-all"
+      ]
 
-    where clangOpts = [ "--target=wasm32"
-                      , "--no-standard-libraries"
-                      , "-O1"
-                      , "-Wl,--no-entry", "-Wl,--export-all", "-Wl,--strip-all" 
-                      ]
-
-withWatFromWasm
-  :: FilePath -- path to .wasm file
-  -> (FilePath -> IO a) -- what to do with .wat file
-  -> IO a
+withWatFromWasm ::
+  FilePath -> -- path to .wasm file
+  (FilePath -> IO a) -> -- what to do with .wat file
+  IO a
 withWatFromWasm wasmFile operation = do
-    withSystemTempDirectory "withWatFromWasm" $ \dir  -> do
-      let watPath = dir </> takeFileName wasmFile <.> "wat"
-      (ex, wasmout, wasmerr) <- readProcessWithExitCode "wasm2wat"
-        [wasmFile, "-o", watPath] ""
-      case ex of
-            ExitSuccess -> operation watPath
-            ExitFailure e ->
-              error $ "wasm2wat failed: " ++ show (e, wasmout, wasmerr)
+  withSystemTempDirectory "withWatFromWasm" $ \dir -> do
+    let watPath = dir </> takeFileName wasmFile <.> "wat"
+    (ex, wasmout, wasmerr) <-
+      readProcessWithExitCode
+        "wasm2wat"
+        [wasmFile, "-o", watPath]
+        ""
+    case ex of
+      ExitSuccess -> operation watPath
+      ExitFailure e ->
+        error $ "wasm2wat failed: " ++ show (e, wasmout, wasmerr)
 
-withWasmFromWat
-  :: FilePath -- path to .wat file
-  -> (WasmFile -> Wasm.Module -> IO a)
-  -> IO a
+withWasmFromWat ::
+  FilePath -> -- path to .wat file
+  (WasmFile -> Wasm.Module -> IO a) ->
+  IO a
 withWasmFromWat watFile operation = do
   watCode <- LBS.readFile watFile
   case Wasm.parse watCode of
-    Left err -> error
-      ("couldn't parse textual WASM from " ++ watFile ++ ": " ++ show err)
+    Left err ->
+      error
+        ("couldn't parse textual WASM from " ++ watFile ++ ": " ++ show err)
     Right wasmMod -> operation (DotWat watFile) wasmMod
 
-withWasmFromC
-  :: FilePath -- path to .c file
-  -> (WasmFile -> Wasm.Module -> IO a)
-  -> IO a
+withWasmFromC ::
+  FilePath -> -- path to .c file
+  (WasmFile -> Wasm.Module -> IO a) ->
+  IO a
 withWasmFromC dotc operation = withCompiledC dotc $ \wasmFile -> do
-    wasmCode <- LBS.readFile wasmFile
-    case Wasm.decodeLazy wasmCode of
-        Left err -> error
-          ("couldn't parse binary WASM from " ++ wasmFile ++ ": " ++ err)
-        Right wasmMod -> operation (DotWasm wasmFile) wasmMod
+  wasmCode <- LBS.readFile wasmFile
+  case Wasm.decodeLazy wasmCode of
+    Left err ->
+      error
+        ("couldn't parse binary WASM from " ++ wasmFile ++ ": " ++ err)
+    Right wasmMod -> operation (DotWasm wasmFile) wasmMod
 
 data WasmFile = DotWasm FilePath | DotWat FilePath
-  deriving Show
+  deriving (Show)
 
-withWasm
-  :: FilePath -- .wat or .c file
-  -> (WasmFile -> Wasm.Module -> IO a)
-  -> IO a
-withWasm fp operation
-  | takeExtension fp == ".c"   = withWasmFromC fp operation
-  | takeExtension fp == ".wat" = withWasmFromWat fp operation
-  | otherwise = error ("cannot handle file: " ++ fp ++ ", only .c and .wat are supported")
+withWasm ::
+  FilePath -> -- .wat or .c file
+  (WasmFile -> Wasm.Module -> IO a) ->
+  IO a
+withWasm fp operation = case takeExtension fp of
+  ".c" -> withWasmFromC fp operation
+  ".wat" -> withWasmFromWat fp operation
+  _ -> error ("cannot handle file: " ++ fp ++ ", only .c and .wat are supported")
 
 ---------------
 
@@ -210,30 +228,30 @@ dumps s xs = dump $ unlines (decorate s xs)
 
 dumpFile :: String -> FilePath -> IO ()
 dumpFile header fp = do
-    s <- readFile fp
-    dumps (header ++ " (" ++ fp ++ ")") (lines s)
+  s <- readFile fp
+  dumps (header ++ " (" ++ fp ++ ")") (lines s)
 
 decorate :: String -> [String] -> [String]
 decorate str lns =
-    [ dottedLine
-    , wrapBars (alignCenter str')
-    , dottedLine
-    ] ++
-    map (wrapBars . padRight) lns ++
-    [ dottedLine ]
+  [ dottedLine,
+    wrapBars (alignCenter str'),
+    dottedLine
+  ]
+    ++ map (wrapBars . padRight) lns
+    ++ [dottedLine]
+  where
+    str' = "\ESC[0;33m" ++ str ++ "\ESC[0m"
+    l = maximum $ map mylength (str' : lns)
+    totalLen = l + 4
+    dottedLine = replicate totalLen '-'
+    wrapBars s = "| " ++ s ++ " |"
+    alignCenter s =
+      let leftPad = (l - mylength s) `div` 2
+          rightPad = (l - mylength s) - leftPad
+       in replicate leftPad ' ' ++ s ++ replicate rightPad ' '
+    padRight s = s ++ replicate (l - mylength s) ' '
 
-  where str' = "\ESC[0;33m" ++ str ++ "\ESC[0m"
-        l = maximum $ map mylength (str':lns)
-        totalLen = l + 4
-        dottedLine = replicate totalLen  '-'
-        wrapBars s = "| " ++ s ++ " |"
-        alignCenter s =
-            let leftPad = (l - mylength s) `div` 2
-                rightPad = (l - mylength s) - leftPad
-            in replicate leftPad ' ' ++ s ++ replicate rightPad ' '
-        padRight s = s ++ replicate (l - mylength s) ' '
-
-        mylength [] = 0
-        mylength ('\ESC':xs) = case dropWhile (/='m') xs of
-          rest -> mylength (tail rest)
-        mylength (_x:xs) = 1 + mylength xs
+    mylength [] = 0
+    mylength ('\ESC' : xs) = case dropWhile (/= 'm') xs of
+      rest -> mylength (tail rest)
+    mylength (_x : xs) = 1 + mylength xs
