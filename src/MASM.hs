9d2fcf5da1680b4769504a10f5a36977805bad31
Qredo LTD <support@qredo.com>
2022-12-20 17:59:14 +0800

    Make ppInstr available

--- src/MASM.hs
+++ src/MASM.hs
@@ -112,91 +112,91 @@ indent = censor (fmap ("  "++))
 
 ppMASM :: Module -> String
 ppMASM = unlines . toList . execWriter . runPpMASM . ppModule
-  where ppModule m = do
-          tell $ DList.fromList $ fmap (("use."++) . unpack) (moduleImports m)
-          traverse_ ppProc (moduleProcs m)
-          ppProgram (moduleProg m)
-        ppProc (name, p) = do
-          [ "proc." ++ T.unpack name ++ "." ++ show (procNLocals p) ]
-          indent $ traverse_ ppInstr (procInstrs p)
-          "end"
-        ppProgram p = do
-          "begin"
-          indent $ traverse_ ppInstr (programInstrs p)
-          "end"
-        ppInstr :: Instruction -> PpMASM ()
-        ppInstr (Exec pname) = [ "exec." ++ unpack pname ]
-        ppInstr (If {thenBranch, elseBranch}) = do
-          "if.true"
-          indent $ traverse_ ppInstr thenBranch
-          unless (null elseBranch) $ do
-            "else"
-            indent $ traverse_ ppInstr elseBranch
-          "end"
-        ppInstr (While body) = do
-          "while.true"
-          indent $ traverse_ ppInstr body
-          "end"
+ppModule m = do
+  tell $ DList.fromList $ fmap (("use."++) . unpack) (moduleImports m)
+  traverse_ ppProc (moduleProcs m)
+  ppProgram (moduleProg m)
+ppProc (name, p) = do
+  [ "proc." ++ T.unpack name ++ "." ++ show (procNLocals p) ]
+  indent $ traverse_ ppInstr (procInstrs p)
+  "end"
+ppProgram p = do
+  "begin"
+  indent $ traverse_ ppInstr (programInstrs p)
+  "end"
+ppInstr :: Instruction -> PpMASM ()
+ppInstr (Exec pname) = [ "exec." ++ unpack pname ]
+ppInstr (If {thenBranch, elseBranch}) = do
+  "if.true"
+  indent $ traverse_ ppInstr thenBranch
+  unless (null elseBranch) $ do
+    "else"
+    indent $ traverse_ ppInstr elseBranch
+  "end"
+ppInstr (While body) = do
+  "while.true"
+  indent $ traverse_ ppInstr body
+  "end"
 
-        ppInstr (LocStore n) = [ "loc_store." ++ show n ]
-        ppInstr (LocLoad n) = [ "loc_load." ++ show n ]
+ppInstr (LocStore n) = [ "loc_store." ++ show n ]
+ppInstr (LocLoad n) = [ "loc_load." ++ show n ]
 
-        ppInstr (AdvPush n) = [ "adv_push." ++ show n ]
-        ppInstr (Push n) = [ "push." ++ show n ]
-        ppInstr (Swap n) = [ "swap" ++ if n == 1 then "" else "." ++ show n ]
-        ppInstr Drop = "drop"
-        ppInstr CDrop = "cdrop"
-        ppInstr (Dup n) = [ "dup." ++ show n ]
-        ppInstr (MoveUp n) = [ "movup." ++ show n ]
-        ppInstr TruncateStack = "exec.sys::truncate_stack"
-        ppInstr SDepth = "sdepth"
-        ppInstr (Eq Nothing) = "eq"
-        ppInstr (Eq (Just n)) = [ "eq." ++ show n ]
-        ppInstr (NEq Nothing) = "neq"
-        ppInstr (NEq (Just n)) = [ "neq." ++ show n ]
-        ppInstr Not = "not"
+ppInstr (AdvPush n) = [ "adv_push." ++ show n ]
+ppInstr (Push n) = [ "push." ++ show n ]
+ppInstr (Swap n) = [ "swap" ++ if n == 1 then "" else "." ++ show n ]
+ppInstr Drop = "drop"
+ppInstr CDrop = "cdrop"
+ppInstr (Dup n) = [ "dup." ++ show n ]
+ppInstr (MoveUp n) = [ "movup." ++ show n ]
+ppInstr TruncateStack = "exec.sys::truncate_stack"
+ppInstr SDepth = "sdepth"
+ppInstr (Eq Nothing) = "eq"
+ppInstr (Eq (Just n)) = [ "eq." ++ show n ]
+ppInstr (NEq Nothing) = "neq"
+ppInstr (NEq (Just n)) = [ "neq." ++ show n ]
+ppInstr Not = "not"
 
-        ppInstr (Add Nothing) = "sub"
-        ppInstr (Add (Just n)) = [ "add." ++ show n ]
-        ppInstr (Sub Nothing) = "sub"
-        ppInstr (Sub (Just n)) = [ "sub." ++ show n ]
+ppInstr (Add Nothing) = "sub"
+ppInstr (Add (Just n)) = [ "add." ++ show n ]
+ppInstr (Sub Nothing) = "sub"
+ppInstr (Sub (Just n)) = [ "sub." ++ show n ]
 
-        ppInstr IAdd = "u32wrapping_add"
-        ppInstr ISub = "u32wrapping_sub"
-        ppInstr IMul = "u32wrapping_mul"
-        ppInstr IDiv = "u32checked_div"
-        ppInstr IMod = "u32checked_mod"
-        ppInstr (IDivMod mk) = [ "u32checked_divmod" ++ maybe "" (\k -> "." ++ show k) mk ]
-        ppInstr IShL = "u32checked_shl"
-        ppInstr IShR = "u32checked_shr"
-        ppInstr (IEq mk) = [ "u32checked_eq" ++ maybe "" (\k -> "." ++ show k) mk ]
-        ppInstr INeq = "u32checked_neq"
-        ppInstr ILt = "u32checked_lt"
-        ppInstr IGt = "u32checked_gt"
-        ppInstr ILte = "u32checked_lte"
-        ppInstr IGte = "u32checked_gte"
-        ppInstr IAnd = "u32checked_and"
-        ppInstr IOr = "u32checked_or"
-        ppInstr IXor = "u32checked_xor"
-        ppInstr INot = "u32checked_not"
+ppInstr IAdd = "u32wrapping_add"
+ppInstr ISub = "u32wrapping_sub"
+ppInstr IMul = "u32wrapping_mul"
+ppInstr IDiv = "u32checked_div"
+ppInstr IMod = "u32checked_mod"
+ppInstr (IDivMod mk) = [ "u32checked_divmod" ++ maybe "" (\k -> "." ++ show k) mk ]
+ppInstr IShL = "u32checked_shl"
+ppInstr IShR = "u32checked_shr"
+ppInstr (IEq mk) = [ "u32checked_eq" ++ maybe "" (\k -> "." ++ show k) mk ]
+ppInstr INeq = "u32checked_neq"
+ppInstr ILt = "u32checked_lt"
+ppInstr IGt = "u32checked_gt"
+ppInstr ILte = "u32checked_lte"
+ppInstr IGte = "u32checked_gte"
+ppInstr IAnd = "u32checked_and"
+ppInstr IOr = "u32checked_or"
+ppInstr IXor = "u32checked_xor"
+ppInstr INot = "u32checked_not"
 
-        ppInstr (MemLoad mi) = [ "mem_load" ++ maybe "" (\i -> "." ++ show i) mi ]
-        ppInstr (MemStore mi) = [ "mem_store" ++ maybe "" (\i -> "." ++ show i) mi ]
-        ppInstr IAdd64 = "exec.u64::wrapping_add"
-        ppInstr ISub64 = "exec.u64::checked_sub"
-        ppInstr IMul64 = "exec.u64::checked_mul"
-        ppInstr IEq64 = "exec.u64::checked_eq"
-        ppInstr INeq64 = "exec.u64::checked_neq"
-        ppInstr IEqz64 = "exec.u64::checked_eqz"
-        ppInstr ILt64 = "exec.u64::checked_lt"
-        ppInstr IGt64 = "exec.u64::checked_gt"
-        ppInstr ILte64 = "exec.u64::checkted_lte"
-        ppInstr IGte64 = "exec.u64::checked_gte"
-        ppInstr IShL64 = "exec.u64::overflowing_shl"
-        ppInstr IShR64 = "exec.u64::overflowing_shr"
-        ppInstr IOr64 = "exec.u64::checked_or"
-        ppInstr IAnd64 = "exec.u64::checked_and"
-        ppInstr IXor64 = "exec.u64::checked_xor"
+ppInstr (MemLoad mi) = [ "mem_load" ++ maybe "" (\i -> "." ++ show i) mi ]
+ppInstr (MemStore mi) = [ "mem_store" ++ maybe "" (\i -> "." ++ show i) mi ]
+ppInstr IAdd64 = "exec.u64::wrapping_add"
+ppInstr ISub64 = "exec.u64::checked_sub"
+ppInstr IMul64 = "exec.u64::checked_mul"
+ppInstr IEq64 = "exec.u64::checked_eq"
+ppInstr INeq64 = "exec.u64::checked_neq"
+ppInstr IEqz64 = "exec.u64::checked_eqz"
+ppInstr ILt64 = "exec.u64::checked_lt"
+ppInstr IGt64 = "exec.u64::checked_gt"
+ppInstr ILte64 = "exec.u64::checkted_lte"
+ppInstr IGte64 = "exec.u64::checked_gte"
+ppInstr IShL64 = "exec.u64::overflowing_shl"
+ppInstr IShR64 = "exec.u64::overflowing_shr"
+ppInstr IOr64 = "exec.u64::checked_or"
+ppInstr IAnd64 = "exec.u64::checked_and"
+ppInstr IXor64 = "exec.u64::checked_xor"
 
-        ppInstr Assert = "assert"
-        ppInstr AssertZ = "assertz"
+ppInstr Assert = "assert"
+ppInstr AssertZ = "assertz"
